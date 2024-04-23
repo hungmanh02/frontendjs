e7e5cb93635a5a5936446256131936a4c9474623
hungmanh02 <domanh462@gmail.com>
2024-04-24 00:09:09 +0700

    Redux : to inittial state  adminReducer

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -1,33 +1,33 @@
 import actionTypes from "../actions/actionTypes";
 
 const initialState = {
-  isLoggedIn: false,
-  adminInfo: null,
+  genders: [],
+  roles: [],
+  positions: [],
 };
 
-const appReducer = (state = initialState, action) => {
+const adminReducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.ADMIN_LOGIN_SUCCESS:
+    case actionTypes.FETCH_GENDER_START:
       return {
         ...state,
-        isLoggedIn: true,
-        adminInfo: action.adminInfo,
       };
-    case actionTypes.ADMIN_LOGIN_FAIL:
+    case actionTypes.FETCH_GENDER_SUCSSES:
       return {
         ...state,
-        isLoggedIn: false,
-        adminInfo: null,
       };
-    case actionTypes.PROCESS_LOGOUT:
+    case actionTypes.FETCH_GENDER_FAILED:
       return {
         ...state,
-        isLoggedIn: false,
-        adminInfo: null,
       };
+    // return {
+    //   ...state,
+    //   isLoggedIn: true,
+    //   adminInfo: action.adminInfo,
+    // };
     default:
       return state;
   }
 };
 
-export default appReducer;
+export default adminReducer;
